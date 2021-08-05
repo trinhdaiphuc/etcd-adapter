ff89839ec4bfee0d863e0d8c7ba38b7b3ead8052
trinhdaiphuc <trinhdaiphuc@gmaill.com>
2021-08-06 00:22:56 +0700

    update casbin v2

--- examples/example.go
+++ examples/example.go
@@ -1,23 +1,30 @@
 package main
 
 import (
-	"github.com/casbin/casbin"
-	"github.com/sebastianliu/etcd-adapter"
+	"fmt"
+	"github.com/casbin/casbin/v2"
+	"github.com/trinhdaiphuc/etcd-adapter"
 )
 
 func main() {
 	// Initialize a casbin etcd adapter and use it in a Casbin enforcer:
 	// The adapter will use the ETCD and a named path with the key you give.
 	// If not provided, the adapter will use the default value casbin_policy.
-	a := etcdadapter.NewAdapter([]string{"http://127.0.0.1:2379"}, "casbin_policy_test") // Your etcd endpoints and the path key.
+	a := etcdadapter.NewAdapter(
+		etcdadapter.WithEndpoints([]string{"http://localhost:2379"}),
+		etcdadapter.WithEtcdAuth("root", "password"),
+		etcdadapter.WithKey("casbin_policy_test"),
+	) // Your etcd endpoints and the path key.
 
-	e := casbin.NewEnforcer("rbac_model.conf", a)
+	e, _ := casbin.NewEnforcer("rbac_model.conf", a)
 
 	// Load the policy from ETCD.
 	e.LoadPolicy()
 
 	// Check the permission.
-	e.Enforce("alice", "data1", "read")
+	result, err := e.Enforce("alice", "data1", "read")
+
+	fmt.Printf("result %v, error %v\n", result, err)
 
 	// Modify the policy.
 	// e.AddPolicy(...)
